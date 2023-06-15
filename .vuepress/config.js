2cf36503838b53afd1abc562cb715645d3128b75
gamnetic <136715105+gamnetic@users.noreply.github.com>
2023-06-15 17:14:49 +0300

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -15,10 +15,10 @@ module.exports = {
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' }],
     ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: 'https://vittominacori.github.io/watch-token' }],
-    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.png' }], // eslint-disable-line max-len
+    ['meta', { property: 'og:url', content: 'https://gamnetic.github.io/gamic' }],
+    ['meta', { property: 'og:image', content: 'https://gamnetic.github.io/gamic/assets/images/watch-token.png' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.png' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:image', content: 'https://gamnetic.github.io/gamic/assets/images/watch-token.png' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'WatchToken | Create a Widget for your ERC20 or BEP20 Token' }],
     ['script', { src: '/assets/js/web3.min.js' }],
     ['script',
